074b86202079a133377c3511bc23f44d3f784fae
hemeshvpatel <hemesh.v.patel@gmail.com>
2020-11-18 17:08:54 -0600

    added app outline and installed material ui

--- src/App.js
+++ src/App.js
@@ -4,6 +4,18 @@ function App() {
   return (
     <div className="app">
       <h1>Covid-19 Tracker</h1>
+
+      {/* Header */}
+      {/* Title + Select input dropdown field */}
+
+      {/* Info Boxes */}
+      {/* Info Boxes */}
+      {/* Info Boxes */}
+
+      {/* Table */}
+      {/* Graph */}
+
+      {/* Map */}
     </div>
   );
 }
